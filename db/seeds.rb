ff02bf45a2a10bc3a52df98bc177157923589954
Nicolas Caif <r.nicolascaif@gmail.com>
2019-02-14 17:27:21 -0300

    add doses routes controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'deleting records'
+Ingredient.destroy_all
+
+puts 'creating new records'
 
 Ingredient.create(name: 'lemon')
 Ingredient.create(name: 'ice')
 Ingredient.create(name: 'mint leaves')
 Ingredient.create(name: 'coke')
+Ingredient.create(name: 'Fernet Branca')
+Ingredient.create(name: 'Rum')
+
+puts 'finished seeding'
