0d521219a3bd414dc2253890c7d96946f6c07783
Nicolas Caif <r.nicolascaif@gmail.com>
2019-02-17 12:39:11 -0300

    changed stylesheets and created Review controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,26 @@
 class ReviewsController < ApplicationController
+  before_action :set_cocktail
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.cocktail = @cocktail
+    if @review.save
+      redirect_to @cocktail
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
 end
