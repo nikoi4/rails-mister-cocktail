4dc37460baaf6388bd009b3e9daef64c588e6fe9
Nicolas Caif <r.nicolascaif@gmail.com>
2019-04-05 18:08:03 -0300

    minor fix in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
-  resources :cocktails, except: [:update, :destroy, :edit] do
+  resources :cocktails, except: [:update, :destroy, :edit, :index] do
     resources :doses, only: [:new, :create]
     resources :reviews, only: [:new, :create]
   end
